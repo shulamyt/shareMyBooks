05e3a469c62ce4a3bdf092b7d90ba01c00fc4ee6
brachan <bracha.neuman@gmail.com>
2016-08-04 21:23:44 +0300

    trying to connect to db

--- server/service/bookService.js
+++ server/service/bookService.js
@@ -1,44 +1,40 @@
+var sql=require('mssql');
+
+
 function BookService(){
 	
 };
-var sql=require('mssql');
 
 var dbConfig={
 	server:"localhost\\MSSQLSERVER",
-	database:"smb_test"
+	port:1433,
+	database:"smb_test",
+	user:"sa",
+	password:"123456"
 };
 
 BookService.prototype.getBook = function(){
 
 	var conn = new sql.Connection(dbConfig);
 	var req = new sql.Request(conn);
-
 	conn.connect(function(err){
 		if(err){
-			console.log(err);
+			console.log(err+' error at connecting to database');
 			return null;
 		}
+		console.log("success to connect");
 		req.query("select * from [dbo].[Book] b where b.bookId='123'",
 			function(err,recordset){
 				if(err){
-					console.log(err);
+					console.log(err+' error with query');
 					return null;
 				}
 				else{
-					return recordset;
+					console.log(recordset);
+					return recordset[0];
 				}
-			})
+			});
 	});
-	
-
-
-
-
-	// var book = {
- //        name: "tttt",
- //        book_id: id
- //    };
-	// return book;
 };
 
 var bookService = new BookService();
