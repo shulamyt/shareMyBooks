b7240797129057981b042ee6b9bc9a198fc4a493
rachellapidot <rachell33815@gmail.com>
2016-11-10 20:56:22 +0200

    add post function

--- app/components/popup.js
+++ app/components/popup.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import SkyLight from 'react-skylight';
+var $ = require("jquery");
+import * as restService from './../service/restService';
+
 
 class Popup extends React.Component {
   constructor(props){
     super(props);
   }
-
+  adduser(){
+    var password=$("#pswrd").val();
+    var f_name=$("#f_n").val();
+    var l_name=$("#l_n").val();
+    var email=$("#eml").val();
+    var phone=$("#phn").val();
+    var address=$("#add").val();
+    var user={
+      "password":password,
+      "f_name":f_name,
+      "l_name":l_name,
+      "email":email,
+      "phone":phone,
+      "address":address
+    }
+    console.log(user);
+    restService.post('/users', user).then(function(fetchProjects){
+    console.log("retrn from server");
+  });
+  }
   render() {
 
     return (
@@ -16,11 +38,13 @@ class Popup extends React.Component {
         <SkyLight hideOnOverlayClicked ref="simpleDialog" title="Welcome to ShareMyBooks">
           <h3>Hello, thank you for joining us!</h3>
           <h4>please enter your details:</h4>
-          *First name: <input type="text" /><br/><br/>
-          *Last name:  <input type="text" /><br/><br/>
-          *Email:      <input type="text" /><br/><br/>
-           Address:    <input type="text" /><br/>
-
+          *password: <input type="text" id="pswrd"/><br/><br/>
+          *First name: <input type="text" id="f_n"/><br/><br/>
+          *Last name:  <input type="text" id="l_n" /><br/><br/>
+          *Email:      <input type="text" id="eml"/><br/><br/>
+           phone:    <input type="text" id="phn"/><br/>
+           Address:    <input type="text" id="add"/><br/>
+           <button onClick={()=>this.adduser()}>add</button>
         </SkyLight>
       </div>
     )
--- app/service/restService.js
+++ app/service/restService.js
@@ -21,3 +21,31 @@ export function get(url) {
 	});
 	return promise;
 }
+export function post(url, user) {
+	var promise =  new Promise(function(resolve, reject) {
+		var request = new XMLHttpRequest();  
+		request.open('POST', url,true);
+		request.setRequestHeader("Content-Type", "application/json");
+		request.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+		request.setRequestHeader("authorization", "UXF_SessionToken:q/Lb0xKRC6F1p39iGViUoA==___currentencryptionkey___nNJ3b59nae52kQ/CQvql8qUfx6tPHuiDIgIlX2RJpvB8d/U6X9ieSdzFS+cgU914EBZ6mP2fYoUu7aC41b9ONOBNU8F1JuFnH6sTygrbjrU=");
+		request.setRequestHeader("X-Requested-With","XMLHttpRequest" );
+		
+		request.onload = function() {
+	  	if (request.status >= 200 && request.status < 400) {
+		    var data = JSON.parse(request.responseText);
+		    resolve(data);
+		  } else {
+		    reject();
+		  }
+		};
+
+		request.onerror = function() {
+		  reject();
+		};
+		
+		request.send(JSON.stringify({user}));
+	});
+	return promise;
+}
+
+var request = new XMLHttpRequest();
