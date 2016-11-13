875f178c51f1d97a21ee1b31c8d6e7dbf242b4f1
brachan <bracha.neuman@gmail.com>
2016-11-13 14:48:17 +0200

    added regex for email validation

--- app/components/popup.js
+++ app/components/popup.js
@@ -8,6 +8,10 @@ class Popup extends React.Component {
   constructor(props){
     super(props);
   }
+  isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+  }
   adduser(){
     var err=false;
     var password=$("#pswrd").val();
@@ -16,6 +20,7 @@ class Popup extends React.Component {
     var email=$("#eml").val();
     var phone=$("#phn").val();
     var address=$("#add").val();
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var filter = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
     if(password=="" || f_name=="" || l_name==""  || address=="" || phone==""){
         $("#spnErr").text("You must fill in required fields!!! :(");
@@ -30,6 +35,13 @@ class Popup extends React.Component {
     }
     else{
       $("#spnphn").text("");
+    }
+    if(email!="" && !regex.test(email)){
+      $("#spneml").text("Valid format only!!!");
+        err=true;
+    }
+    else{
+      $("#spneml").text("");
     }
     if (!err){
       var user={
@@ -60,9 +72,9 @@ class Popup extends React.Component {
           *password: <input type="text" id="pswrd"/><br/><br/>
           *First name: <input type="text" id="f_n"/><br/><br/>
           *Last name:  <input type="text" id="l_n" /><br/><br/>
-          *Email:      <input type="text" id="eml"/><br/><br/>
-           phone:    <input type="text" id="phn"/><span id="spnphn"></span><br/><br/>
-           Address:    <input type="text" id="add"/><br/><br/>
+          Email:      <input type="text" id="eml"/><span id="spneml"></span><br/><br/>
+          *phone:    <input type="text" id="phn"/><span id="spnphn"></span><br/><br/>
+          *Address:    <input type="text" id="add"/><br/><br/>
            <span id="spnErr"></span><br/><br/>
            <button onClick={()=>this.adduser()}>add</button>
         </SkyLight>
