c7e6e9721aad2e563c5badef5f8c6a203f7f4efb
rachellapidot <rachellapidot@users.noreply.github.com>
2017-06-15 16:33:39 +0300

    fetch data

--- app/components/alertsArea.js
+++ app/components/alertsArea.js
@@ -6,23 +6,41 @@ import * as restService from './../service/restService';
 class AlertsArea extends React.Component {
 	constructor(props){
 		super(props);
-		var _this=this;
-		this.state = {
-			'LateBooksData': _this.getAlertedBooks("/loans/late/"+_this.props.userId),
-			'WaitingBooksData':_this.getAlertedBooks("/loans/waiting/"+_this.props.userId)
-		}
+//  		var _this=this;
+		this.getAlertedBooks("/loans/late/"+this.props.userId, 'late')
+		this.getAlertedBooks("/loans/waiting/"+this.props.userId, 'wait')
 	}
+	
+// 	this.state = {
+// 			'LateBooksData': _this.getAlertedBooks("/loans/late/"+_this.props.userId),
+// 			'WaitingBooksData':_this.getAlertedBooks("/loans/waiting/"+_this.props.userId)
+// 		}
+	
+	this.state = {
+			LateBooksData:[] ,
+			WaitingBooksData:[]
+		}
 
-	getAlertedBooks(path){
+	getAlertedBooks(path, state){
 		let thisVal=this;
 		var thisProps=this.props;
 		restService.get(path).then(function(fetchBooks){
 			console.log(fetchBooks);
 			if(fetchBooks){
-				return fetchBooks;
+				if(state==='wait'){
+					this.setState({WaitingBooksData:fetchBooks})
+				}
+				else{
+					this.setState({LateBooksData:fetchBooks})
+				}
 			}
 			else
-				return [];
+				if(state==='wait'){
+					this.setState({WaitingBooksData:[]})
+				}
+				else{
+					this.setState({LateBooksData:[]})
+				}
 		});
 	}
 	render() {
